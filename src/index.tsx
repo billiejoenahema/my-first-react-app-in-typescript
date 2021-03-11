3f5ee9eaafd9f5c3690eea9997165922d7c9b805
billiejoenahema <billiejoenahema@gmail.com>
2021-03-11 21:04:04 +0900

    add: message to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App message="Helle, React!" />,
+    document.getElementById('root')
+)
